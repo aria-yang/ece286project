7666880224d328fdac91f7e5c6433c431835c3dc
aria-yang <aria.yang@mail.utoronto.ca>
2025-02-17 20:42:23 -0500

    Randomized 5x5 grid of cards when you press new game

--- src/App.js
+++ src/App.js
@@ -1,11 +1,65 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react'
+
+const cardImages = [{"src": "/img/0.png"}, 
+  { "src": "/img/1.png" }, 
+  { "src": "/img/2.png" }, 
+  { "src": "/img/3.png" }, 
+  { "src": "/img/4.png" }, 
+  { "src": "/img/5.png" }, 
+  { "src": "/img/6.png" }, 
+  { "src": "/img/7.png" }, 
+  { "src": "/img/8.png" }, 
+  { "src": "/img/9.png" }, 
+  { "src": "/img/Blank.png" },
+  { "src": "/img/Blank.png" },
+  { "src": "/img/Blank.png" },
+  { "src": "/img/Blank.png" },
+  { "src": "/img/Blank.png" },
+  { "src": "/img/Blank.png" },
+  { "src": "/img/Blank.png" },
+  { "src": "/img/Blank.png" },
+  { "src": "/img/Blank.png" },
+  { "src": "/img/Blank.png" },
+  { "src": "/img/Blank.png" },
+  { "src": "/img/Blank.png" },
+  { "src": "/img/Blank.png" },
+  { "src": "/img/Blank.png" },
+  { "src": "/img/Blank.png" }
+]
 
 function App() {
+  const [cards, setCards] = useState([])
+  const [turns, setTurns] = useState(0)
+
+  // shuffle cards
+  const shuffleCards = () => {
+    const shuffledCards = [...cardImages]
+      .sort(() => Math.random() - 0.5)
+      .map((card) => ({...card, id: Math.random() }))
+
+    setCards(shuffledCards)
+    setTurns(0)
+  }
+
+  console.log(cards, turns)
+
   return (
     <div className="App">
       <h1>Number Game</h1>
-      <button>Start Game</button>
+      <button onClick={shuffleCards}>Start Game</button>
+
+      <div className="card-grid">
+        {cards.map(card => (
+          <div className="card" key={card.id}>
+            <div>
+              <img className="front" src={card.src} alt="card front" />
+              <img className="back" src="/img/cover.png" alt="" alt="card back" />
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
